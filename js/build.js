a3e8faaa06f53a7825b70ab8b157d825fded54e2
Joery van Egmond <45428077+joeryvanegmond@users.noreply.github.com>
2020-02-26 20:04:02 +0100

    added empty field

--- js/build.js
+++ js/build.js
@@ -8,21 +8,26 @@ class warehouse
     //functions
 }
   
-  
-  
-  
   // function that builds a grid in the "container"
    function createGrid() {
         var x = 15;
     for (var rows = 0; rows < x; rows++) {
         for (var columns = 0; columns < x; columns++) {
-            document.getElementById("grid").append("<div class='grid-item'></div>");
+            var node = document.createElement("div");
+            node.classList.add("grid-item");
+            document.getElementById("grid").appendChild(node);
         };
     };
-    let gridItems = document.getElementsByClassName("grid-item");
+    let gridItems = document.getElementsByClassName('grid-item');
 
-    gridItems.forEach(element => {
-        element.width(960/x);
-        element.height(960/x);
+    // for (let index = 0; index < gridItems.length; index++) {
+    //     const element = array[index];
+    //     element.width(960/x);
+    //     element.height(960/x);
+    // }
+
+    Array.from(gridItems).forEach(element => {
+        element.style.width = 960 / x;
+        element.style.height = 960 / x;
     });
 };
